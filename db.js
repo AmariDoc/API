f6067cc5e1bc5777926a78128d477311c66e17f9
Amari Daugherty <123038436+AmariDoc@users.noreply.github.com>
2024-04-22 20:59:38 -0400

    modified db config for render

--- db.js
+++ db.js
@@ -1,9 +1,13 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    host: "LocalHost",
-    password:"1234",
+    //user: "postgres",
+    user: "adaugh",
+    host: "dpg-coio130l5elc73daubn0-a",
+    database: "nba_hhv1",
+    password: "MZrVZm8Nvl9GeO6S1c2cYRHEBLNmbcPR",
+    //host: "LocalHost",
+    //password:"1234",
     database: "NBA",
     port: 5432,
 });
