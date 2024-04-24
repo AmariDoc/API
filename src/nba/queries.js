6ba1203d249394b0391388b3b97f9590e48fc2da
Amari Daugherty <123038436+AmariDoc@users.noreply.github.com>
2024-04-23 23:49:03 -0400

    Update queries.js
    Added new GET methods

--- src/nba/queries.js
+++ src/nba/queries.js
@@ -1,5 +1,11 @@
 const getNBA = "SELECT * FROM basketball";
+const getNBAById = "SELECT * FROM basketball WHERE id = $1";
+const addBasketball = "INSERT into basketball (id,player,team) VALUES($1, $2, $3)";
+const updateNBA = "UPDATE basketball set team = $1 where id = $2";
 
 module.exports = {
     getNBA,
+    getNBAById,
+    addBasketball,
+    updateNBA
 };
