ce0b57c9adf220ef60521182f40a6e2c07adc1e7
Amari Daugherty <123038436+AmariDoc@users.noreply.github.com>
2024-04-23 23:47:52 -0400

    Update controller.js
    Updated files according to module 5 lecture

--- src/nba/controller.js
+++ src/nba/controller.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 const pool = require('../../db');
 const queries = require('./queries');
 
@@ -9,10 +10,46 @@ const getNBA = (req, res) => {
     });
 };
 
+//Get NBA By ID
+const getNBAById = ( req,res) => {
+    const id = parseInt(req.params.id);
+    pool.query(queries.getNBAById,[id], (error, results)=> {
+        if(error) throw error;
+        res.status(200).json(results.rows);
+    });
+}
+
+
+//Add Basketball
+const addBasketball = ( req,res) => {
+    const {id, player, team} = req.body;
+    pool.query(queries.addBasketball,[id, player, team], (error, results)=> {
+        if(error) throw error;
+        res.status(200).send("Added New Basketball Stat successfully");
+    });
+}
+
+//update Nba
+const updateNBA =(req,res) => {
+    //const id = parseInt(req.params.id);
+    const { team, id } = req.body;
+    pool.query(queries.updateNBA, [team, id], (error,results) => {
+        if(error) throw error;
+        res.status(200).send("updated Basketball stats successfully");
+    });
+
+}
+
+
+
+
 //Get Data by Param
 //Add new Data by Param
 //Modify Data by Param
 
 module.exports = {
     getNBA,
+    getNBAById,
+    addBasketball,
+    updateNBA,
 };
